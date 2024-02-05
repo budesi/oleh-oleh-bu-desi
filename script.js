5d32aa398c72bc57d56536362a34f1ab0546d1d8
budesi <159011643+budesi@users.noreply.github.com>
2024-02-05 23:50:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu-navigation");
+
+menuBar.addEventListener('click', function(){
+    menuNav.classList.toggle("menu-active")
+})
+
